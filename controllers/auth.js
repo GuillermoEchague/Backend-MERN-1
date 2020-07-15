3c1af8e8b41532c40917925e66aa72b74fc4d0e3
Guillermo <guillermo.echaguea@gmail.com>
2020-07-15 18:27:09 -0400

    Desarrollo de backend con NODE-Express-Mongo para MERN

---
+++ controllers/auth.js
@@ -0,0 +1,126 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+const crearUsuario = async (req, res = response) => {
+
+    const { name, email, password } = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({ email });
+        //  console.log(usuario);
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            });
+        }
+
+
+        usuario = new Usuario(req.body);
+
+
+        // Encriptar Contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+
+        await usuario.save();
+
+        // Generar JWT
+        const token = generarJWT(usuario.id, usuario.name);
+
+
+
+
+        console.log('Se requiere /');
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error),
+            res.status(500).json({
+                ok: false,
+                msg: 'Por favor hable con el administrador'
+            });
+    }
+
+
+
+
+}
+
+
+const loginUsuario = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+        let usuario = await Usuario.findOne({ email });
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            });
+        }
+
+        // Confirmar los password
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            });
+        }
+
+        // Generar nuestro JWT
+        const token = await generarJWT(usuario.id, usuario.name);
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+            token
+
+        })
+
+
+
+
+    } catch (error) {
+        console.log(error),
+            res.status(500).json({
+                ok: false,
+                msg: 'Por favor hable con el administrador'
+            });
+    }
+
+
+}
+
+const revalidarToken = async (req, res = response) => {
+
+    const { uid, name } = req;
+
+
+    console.log('Se requiere /');
+    // Generar nuestro JWT
+    const token = await generarJWT(uid, name);
+    res.json({
+        ok: true,
+        uid, name,
+        token
+    })
+}
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
